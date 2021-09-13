f8dd4ddfed6778af33660193250e92f043d2394b
Vinicius Costa <viniciusath@hotmail.com>
2021-09-13 08:44:10 -0300

    iniciando ejs

--- Projeto1/index.js
+++ Projeto1/index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const app = express();
-//const ejs = require('ejs');
 
-app.get('/', (req, res) => {
-    res.send("Bem vindo ao meu site");
+app.set('view engine', 'ejs');
+
+app.get('/', (req,res) => {
+    res.send(('Bem vindo a index'));
+});
+
+app.get('/:nome/:linguagem', (req, res) => {
+    var nome = req.params.nome;
+    var linguagem = req.params.linguagem;
+    var exibirMensagem = false;
+
+    var produtos = [
+        {nome: 'Doritos', preco: 3.14},
+        {nome: 'Coca-Cola', preco: 5},
+        {nome: 'Leite', preco: 1.45}
+    ]
+
+    res.render('index', {
+        nome: nome,
+        linguagem: linguagem,
+        empresa: 'UNIMES',
+        inscritos: 1000,
+        mensagem: exibirMensagem
+    });
 });
 
 app.listen(4000, () => {
