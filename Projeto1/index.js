7d3876e6dbdb00cae4ab6657f16fcfd123de1b96
Vinicius Costa <viniciusath@hotmail.com>
2021-09-29 14:23:47 -0300

    a

--- Projeto1/index.js
+++ Projeto1/index.js
@@ -13,7 +13,7 @@ connection
     })
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static('public')); 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
